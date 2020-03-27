743460fe693048a6d764aa796f1e8c461fd2e0a5
Jakub Cisło <cubix651@gmail.com>
2020-03-27 22:32:58 +0100

    Add small optimized example 2

---
+++ explicit_instantiation/small_example/optimized_2/a.cpp
@@ -0,0 +1,7 @@
+#include "a.h"
+#include "library.h"
+
+int function_a(int x)
+{
+    return library_function(x) + 1;
+}
---
+++ explicit_instantiation/small_example/optimized_2/main.cpp
@@ -0,0 +1,9 @@
+#include "a.h"
+#include "b.h"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << (function_a(10) + function_b(20)) << std::endl;
+}
